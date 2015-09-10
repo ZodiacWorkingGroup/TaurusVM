ddfb608fa78c7d260b89e81f54424cb5bee799a6
Nathan <nathantronnes@gmail.com>
2015-09-10 10:24:11 -0800

    HALT instruction and CATCH
    For ending the program and returning exit codes

--- Executers/indeterminant/v05.py
+++ Executers/indeterminant/v05.py
@@ -1,5 +1,19 @@
 __author__ = 'Zodiac Working Group'
 
 
+class Programme:
+    def __init__(self, prog):
+        self.i = 0
+        self.raw = prog
+
+    def read(self, n):
+        r = tuple(self.raw[i:i+n])
+        self.i += n
+        return r
+
+    def setloc
+
 def main(prog):
-    pass
+    prog = Programme(prog)
+    while prog.finished == False:
+        pass
