45aa79a7ce31d052fa69f57d97992d0a5b7b9aad
Nathan <nathantronnes@gmail.com>
2015-09-11 11:47:31 -0800

    Rearranged some files

--- Executers/indeterminant/v05.py
+++ Executers/indeterminant/v05.py
@@ -6,13 +6,6 @@ class Programme:
         self.i = 0
         self.raw = prog
 
-    def read(self, n):
-        r = tuple(self.raw[i:i+n])
-        self.i += n
-        return r
-
-    def setloc
-
 def main(prog):
     prog = Programme(prog)
     while prog.finished == False:
