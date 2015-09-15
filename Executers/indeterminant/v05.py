674e5e974e9d3583670e0e148ff10b29350b7b79
Nathan <nathantronnes@gmail.com>
2015-09-15 13:29:55 -0800

    Float instructions

--- Executers/indeterminant/v05.py
+++ Executers/indeterminant/v05.py
@@ -1,6 +1,16 @@
-import getch
+import math
+import struct
+# import getch
 from functools import reduce
 __author__ = 'Zodiac Working Group'
+
+
+def binfromfloat(value):
+    return struct.unpack('Q', struct.pack('d', value))[0]
+
+
+def bintofloat(bits):
+    return struct.unpack('d', struct.pack('Q', bits))[0]
 
 
 class Programme:
@@ -34,7 +44,7 @@ class Programme:
         return len(self.instructions)
 
 
-class regmap():
+class RegMap:
     def __init__(self):
         self.map = {}
 
@@ -47,14 +57,14 @@ class regmap():
     def __setitem__(self, item, value):
         self.map[item] = value
 
-    def __delitem__(self, item, value):
+    def __delitem__(self, item):
         del self.map[item]
 
     def __str__(self):
         return 'regarray: '+str(self.map)
 
 
-def tonum(s):
+def tonum(s, rtype=int):
     def tobin(t):
         return bin(t)[2:].zfill(8)
 
@@ -62,12 +72,17 @@ def tonum(s):
     for x in s:
         r += tobin(ord(x))
 
-    return int(r, 2)
+    r = int(r, 2)
+
+    if rtype == float:
+        r = bintofloat(r)
+
+    return r
 
 
 def main(prog):
     prog = Programme(prog.split('\x00', 0)[0])
-    regs = regmap()
+    regs = RegMap()
 
     i = 0
     while i < len(prog):
@@ -95,25 +110,92 @@ def main(prog):
                     regs[x] = regs[regs[startregnum]]
 
             elif comb == '\x03':
-                regs[tonum(args[0])] = reduce(lambda x, y: x + y, regs[1:], 1)
+                regs[tonum(args[0])] = reduce(lambda y, z: y + z, [regs[y] for y in args[1:]], 1)
 
             elif comb == '\x04':
-                regs[tonum(args[0])] = reduce(lambda x, y: x - y, regs[1:], 1)
+                regs[tonum(args[0])] = reduce(lambda y, z: y - z, [regs[y] for y in args[1:]], 1)
 
             elif comb == '\x05':
-                regs[tonum(args[0])] = reduce(lambda x, y: x * y, regs[1:], 1)
+                regs[tonum(args[0])] = reduce(lambda y, z: y * z, [regs[y] for y in args[1:]], 1)
 
             elif comb == '\x06':
-                regs[tonum(args[0])] = reduce(lambda x, y: x // y, regs[1:], 1)
+                regs[tonum(args[0])] = reduce(lambda y, z: y // z, [regs[y] for y in args[1:]], 1)
 
             elif comb == '\x07':
-                regs[tonum(args[0])] = reduce(lambda x, y: x ** y, regs[1:], 1)
+                regs[tonum(args[0])] = reduce(lambda y, z: y ** z, [regs[y] for y in args[1:]], 1)
 
+            elif comb == '\x08':
+                regs[tonum(args[0])] = binfromfloat(reduce(lambda y, z: y + z, [bintofloat(y) for y in [regs[y] for y in args[1:]]], 1))
 
+            elif comb == '\x09':
+                regs[tonum(args[0])] = binfromfloat(reduce(lambda y, z: y - z, [bintofloat(y) for y in [regs[y] for y in args[1:]]], 1))
 
-        i+=1
-        
+            elif comb == '\x0A':
+                regs[tonum(args[0])] = binfromfloat(reduce(lambda y, z: y * z, [bintofloat(y) for y in [regs[y] for y in args[1:]]], 1))
+
+            elif comb == '\x0B':
+                regs[tonum(args[0])] = binfromfloat(reduce(lambda y, z: y / z, [bintofloat(y) for y in [regs[y] for y in args[1:]]], 1))
+
+            elif comb == '\x0C':
+                regs[tonum(args[0])] = binfromfloat(reduce(lambda y, z: y ** z, [bintofloat(y) for y in [regs[y] for y in args[1:]]],1))
+
+            elif comb == '\x0D':
+                regs[tonum(args[0])] = binfromfloat(math.sqrt(regs[args[1]]))
+
+            elif comb == '\x0E':
+                regs[tonum(args[0])] = binfromfloat(math.sin(regs[args[1]]))
+
+            elif comb == '\x0F':
+                regs[tonum(args[0])] = binfromfloat(math.cos(regs[args[1]]))
+
+            elif comb == '\x10':
+                regs[tonum(args[0])] = binfromfloat(math.tan(regs[args[1]]))
+
+            elif comb == '\x11':
+                regs[tonum(args[0])] = binfromfloat(math.asin(regs[args[1]]))
+
+            elif comb == '\x12':
+                regs[tonum(args[0])] = binfromfloat(math.acos(regs[args[1]]))
+
+            elif comb == '\x13':
+                regs[tonum(args[0])] = binfromfloat(math.atan(regs[args[1]]))
+
+            elif comb == '\x14':
+                regs[tonum(args[0])] = binfromfloat(math.sinh(regs[args[1]]))
+
+            elif comb == '\x15':
+                regs[tonum(args[0])] = binfromfloat(math.cosh(regs[args[1]]))
+
+            elif comb == '\x16':
+                regs[tonum(args[0])] = binfromfloat(math.tanh(regs[args[1]]))
+
+            elif comb == '\x17':
+                regs[tonum(args[0])] = binfromfloat(math.asinh(regs[args[1]]))
+
+            elif comb == '\x18':
+                regs[tonum(args[0])] = binfromfloat(math.acosh(regs[args[1]]))
+
+            elif comb == '\x19':
+                regs[tonum(args[0])] = binfromfloat(math.atanh(regs[args[1]]))
+
+            elif comb == '\x1A':
+                pass
+
+            elif comb == '\x1B':
+                pass
+
+            elif comb == '\x1C':
+                pass
+
+            elif comb == '\x1D':
+                pass
+
+        i += 1
+
     return regs
 
 if __name__ == '__main__':
     print(main(open('SETinstructionexamplecompiled.tau').read()))
+    print(bintofloat(1283))
+    print(binfromfloat(bintofloat(1283)))
+    input()
