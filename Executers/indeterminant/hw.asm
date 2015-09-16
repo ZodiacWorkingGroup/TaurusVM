846fbb68fdc9f2cbad7b95274c9a9cd9c736fd46
Nathan <nathantronnes@gmail.com>
2015-09-15 16:09:59 -0800

    Changed .tau to .taurus
    In the future, .tau will be the archive format

--- Executers/indeterminant/hw.asm
+++ Executers/indeterminant/hw.asm
@@ -1,2 +1,2 @@
-SET R[1] "Hello, World!\n"; Set the registries starting with the first/second to Hello, World
+SET R[1] "Hello, World!\n\0"; Set the registries starting with the first/second to Hello, World\n\0
 PRINTS R[1] ; Print a null-terminated string from R[1] (which now contains the proper string for that)
