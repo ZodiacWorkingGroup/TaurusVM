e15561c6ffb76a9aee8bb06356d2da558d37953b
Nathan <nathantronnes@gmail.com>
2015-09-23 10:16:51 -0800

    Mostly documentation

---
+++ docs/IndeterminantVM/instruction_blocks.md
@@ -0,0 +1,5 @@
+# TaurusVM IndeterminantVM Instruction Blocks
+Instructions on the TaurusVM are divided into blocks. Each block is 256 instructions long, as an instruction is denoted by a character.
+
+The block an instruction is in is denoted by the first character of its instruction. This document lays out these blocks.
+
--- docs/IndeterminantVM/numberfy.py
+++ docs/IndeterminantVM/numberfy.py
@@ -2,15 +2,25 @@
 # number codes. It makes it easier for the authors to add new instructions by inserting,
 # and inserts character codes that cannot be typed
 
-snn = open('setdocs_no_numbers.txt').read()
+import os.path
 
-x = 0
-y = 0
-while '$v' in snn:
-    while len(hex(y)[2:]) < 3:
-        snn = snn.replace('$v', (hex(x)[2:].zfill(2)+hex(y)[2:].zfill(2)).upper(), 1).replace('$c', chr(x)+chr(y), 1)
-        y += 1
-    y = 0
-    x += 1
 
-open('setdocs.txt', 'w').write(snn)
+def curindex(fs):
+    fs = hex(fs)
+    if len(fs[2:]) == 1:
+        return '0x0'+fs[2:]
+    else:
+        return fs
+
+for filesuffix in range(256):
+    if os.path.isfile('setdocs_'+curindex(filesuffix)+'_no_numbers.txt'):
+        snn = open('setdocs_'+curindex(filesuffix)+'_no_numbers.txt').read()
+
+        y = 0
+        while '$v' in snn:
+            while len(hex(y)[2:]) < 3:
+                snn = snn.replace('$v', (hex(filesuffix)[2:].zfill(2)+hex(y)[2:].zfill(2)).upper(), 1).replace('$c', chr(filesuffix)+chr(y), 1)
+                y += 1
+            y = 0
+
+        open('setdocs_'+curindex(filesuffix)+'.txt', 'w').write(snn)
